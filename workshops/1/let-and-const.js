267fb11f9c6708696c7a8d01e08c9d6f5f7ea380
Matt Norrish <matt@mnorrish.co.nz>
2016-09-12 12:53:30 +1200

    use template literals for console logs in let-and-const

--- workshops/1/let-and-const.js
+++ workshops/1/let-and-const.js
@@ -2,17 +2,17 @@ var v = 0; // old-fashioned variable
 let l = 0; // new, block-scoped variable
 const c = 0; // new, block-scoped constant
 
-console.log('var:', v);
-console.log('let:', l);
-console.log('const:', c);
+console.log(`var: ${ v }`);
+console.log(`let: ${ l }`);
+console.log(`const: ${ c }`);
 
 // re-assign the variables
 v = 1;
 l = 1;
 
 // the variables now point to different values
-console.log('var:', v);
-console.log('let:', l);
+console.log(`var: ${ v }`);
+console.log(`let: ${ l }`);
 
 // attempt to re-assign the constant
 c = 1; // throws a TypeError
