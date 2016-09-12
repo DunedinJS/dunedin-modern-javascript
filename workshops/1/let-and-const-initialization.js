c506de4d83d398a304f5ee3041eae8bc9b33305c
Matt Norrish <matt@mnorrish.co.nz>
2016-09-12 13:31:09 +1200

    Update and rename let-and-const-initialization.js to let-and-const-initialization.nd

--- workshops/1/let-and-const-initialization.js
+++
@@ -1,9 +0,0 @@
-// in existing JavaScript it's fine to declare a variable without initializing its value
-var v;
-
-// the same applies for variables declared with let
-let l;
-
-// but const must be initialized on declaration
-const c1 = true; // no problem
-const c2; // throws a SyntaxError
